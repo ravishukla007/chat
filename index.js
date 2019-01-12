8e638a3dfa5e6abff7d02fa8be1958466d340346
Ravi shukla <ravishkl5@gmail.com>
2019-01-12 12:29:21 +0530

    Version 2.0.4

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ chat.init({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'chat'
+  database : 'chat',
+  assets   : 'assets'
 });
 
 app.listen(8080);
 
+ 
+console.log('Server started at 8080');
+console.log('Now you have two end point: http://localhost:8080/index.html');
+console.log('Second end point is http://localhost:8080/index2.html');
+console.log('Open both end point in different browser and your chat is ready to deploy');
+console.log('Have fun :)');
+
 
 function handler (req, res) {
-
-  console.log('request starting...');
 
     var filePath = '.' + req.url;
     if (filePath == './')
